45ccf031fbfbcf781328dda9b70edc661bbd4245
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-05-19 14:16:07 +0900

    빈 Fragment 생성

---
+++ app/src/main/java/com/ssafy/smartstore/view/intro/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.smartstore.view.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.smartstore.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
