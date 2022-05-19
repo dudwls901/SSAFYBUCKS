6d7fcbbb349ce6826c637432ab9402947ac29327
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-05-19 15:07:10 +0900

    LoginActivity -> LoginFragment로 변경

--- app/src/main/java/com/ssafy/smartstore/view/intro/IntroActivity.kt
+++ app/src/main/java/com/ssafy/smartstore/view/intro/IntroActivity.kt
@@ -1,12 +1,59 @@
 package com.ssafy.smartstore.view.intro
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import com.google.firebase.messaging.FirebaseMessaging
 import com.ssafy.smartstore.R
+import com.ssafy.smartstore.databinding.ActivityIntroBinding
+import com.ssafy.smartstore.view.home.HomeActivity
+
+private const val TAG = "IntroActivity"
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // FCM 노티 채널 설정
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            setFCMChannel()
+        }
+    }
+
+    // FCM 노티 채널 설정
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setFCMChannel() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+            if (!it.isSuccessful) {
+                Log.d(TAG, "실패: ${it.exception}")
+            } else {
+                Log.d(TAG, "성공: ${it.result}")
+            }
+        }
+        createNotiChannel("ssafy_id", "ssafy")
+    }
+
+    //오레오 이상에서만 동작하는 함수
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotiChannel(channelId: String, channelName: String) {
+
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelId, channelName, importance)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
     }
 }
--- app/src/main/java/com/ssafy/smartstore/view/intro/LoginFragment.kt
+++ app/src/main/java/com/ssafy/smartstore/view/intro/LoginFragment.kt
@@ -1,59 +1,138 @@
 package com.ssafy.smartstore.view.intro
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.ssafy.smartstore.R
+import com.ssafy.smartstore.data.local.dto.User
+import com.ssafy.smartstore.data.remote.repository.UserRepository
+import com.ssafy.smartstore.databinding.FragmentLoginBinding
+import com.ssafy.smartstore.view.home.HomeActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val TAG = "LoginFragment"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class LoginFragment : Fragment(), CoroutineScope {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var navController: NavController
+
+    private val job = Job()
+    override val coroutineContext = Dispatchers.Main + job
+
+    private lateinit var prefs: SharedPreferences
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        prefs = requireActivity().getSharedPreferences("data", AppCompatActivity.MODE_PRIVATE)
+
+        //자동로그인
+        autoLogin()
+
+        // 뷰 초기화
+        initViews(view)
+    }
+
+    //자동로그인
+    private fun autoLogin() {
+        val id = prefs.getString("id", "")!!
+        val pw = prefs.getString("password", "")!!
+        if (!id.equals("") && !pw.equals("")) login(id, pw)
+    }
+
+    private fun initViews(view: View) = with(binding) {
+
+        navController = Navigation.findNavController(view)
+
+        // 로그인 버튼
+        btnLogin.setOnClickListener {
+            val id = etID.text.toString()
+            val pw = etPW.text.toString()
+            login(id, pw)
+        }
+
+        // 조인 버튼
+        btnJoin.setOnClickListener {
+            navController.navigate(R.id.action_loginFragment_to_joinFragment)
+        }
+    }
+
+    // 로그인 수행
+    private fun login(id: String, pw: String) {
+
+        if (id.isNotEmpty() && pw.isNotEmpty()) {
+
+            launch {
+                val user = UserRepository.INSTANCE.login(User(id, pw)).body()
+                Log.d(TAG, "login: ${user}")
+                if (user == null) {
+                    launch(Dispatchers.Main) {
+                        Toast.makeText(
+                            requireContext(),
+                            "아이디, 비밀번호를 확인해주세요",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else {
+                    // Preferences에 유저정보 저장
+                    val editor = prefs.edit()
+                    editor.putString("id", user.id)
+                    editor.putString("name", user.name)
+                    editor.putString("password", user.pass)
+                    editor.commit()
+
+                    launch(Dispatchers.Main) {
+                        Toast.makeText(requireContext(), "로그인 성공", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+
+                    Intent(
+                        requireActivity(),
+                        HomeActivity::class.java
+                    ).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(this)
+                    }
+
+                }
+            }
+        } else {
+            launch(Dispatchers.Main) {
+                Toast.makeText(requireContext(), "아직 입력되지 않은 항목이 있습니다", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             LoginFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
